c28da1bb963ce40429ccc59cfd554b1c435d0623
이동섭 <dlehdtjq00w@naver.com>
2021-09-10 14:43:36 +0000

    2021-09-10 23:43

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,13 +11,13 @@ const Campground = require('../models/campground');
 
 router.route('/')
 	.get(catchAsync(campgrounds.index))
-	.post(isLoggedIn, validateCampground, upload.array('image'), catchAsync(campgrounds.createCampground));
+	.post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
 	.get(catchAsync(campgrounds.showCampground))
-	.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+	.put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
 	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
--- seeds/index.js
+++ seeds/index.js
@@ -24,12 +24,23 @@ const seedDB = async()=>{
 		const random1000 = Math.floor(Math.random() * 1000);
 		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
+			//YOUR USER ID, if delete user info -> problem occurs
 			author: '613872495c22911c99dc07c0',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
-			image: 'https://source.unsplash.com/collection/483251',
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			price //price: price
+			price, //price: price
+			geometry: {type: "Point", coordinates: [-113.1331, 47.0202]},
+			images: [
+				{
+					url: 'https://res.cloudinary.com/dgd1vqykn/image/upload/v1631246975/YelpCamp/cstq60fhdgbdjyvpdp5h.jpg',
+					filename: 'YelpCamp/cstq60fhdgbdjyvpdp5h'
+				},
+				{
+					url: 'https://res.cloudinary.com/dgd1vqykn/image/upload/v1631246975/YelpCamp/ntns32asydhaut1c4ilq.jpg',
+					filename: 'YelpCamp/ntns32asydhaut1c4ilq'
+				}
+			]
 		})
 		await camp.save();
 	}
